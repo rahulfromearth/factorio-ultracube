74031698cef6d441714a3d559662d387a8c43464
Rahul <rrrexter@gmail.com>
2026-06-24 21:31:51 +0530

    Replace assembling_machine_modules with crafter_modules Replace furnace_modules with crafter_modules Replace rocket_silo_modules with crafter_modules

--- prototypes/entities/electric_furnace.lua
+++ prototypes/entities/electric_furnace.lua
@@ -29,7 +29,7 @@ data:extend({
     fluid_boxes_off_when_no_fluid_recipe = true,
     module_slots = 2,
     icons_positioning = {{
-      inventory_index = defines.inventory.assembling_machine_modules,
+      inventory_index = defines.inventory.crafter_modules,
       shift = {0, 0.8},
     }},
     allowed_effects = module_effects.all,
